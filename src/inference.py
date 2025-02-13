13b3fa23dec007564c8b0c48233d7e5c58cafd02
Nikola Jovisic <nikolajovisic4@gmail.com>
2025-02-13 17:24:26 +0000

    refactor paths, fix png name when saving

--- src/inference.py
+++ src/inference.py
@@ -8,32 +8,29 @@ from models.ddpm_classifier_free import Unet as Unet_class
 from utils.image_utils import save_image_to_dir, save_patches_to_dir
 from utils.model_utils import (load_model, load_classifier_free_model, generate_whole_image, create_lcl_ctx_channels, 
                                create_inputs, generate_patches, stitch_patches, create_patch_channels)
-from config import IS_COND, OVERLAP, MID_IMAGE_SIZE, FINAL_IMAGE_SIZE, timesteps
+from config import IS_COND, OVERLAP, MID_IMAGE_SIZE, FINAL_IMAGE_SIZE, timesteps, WH_PATH, LC_PATH, PH_PATH, SAVE_DIR
 
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # load model for generating whole images in resolution 256x256
-    whole_img_model_path = '../models/artifacts/vindr_healthy_256:v82/model_124499.pt'
     if IS_COND:
-        whole_image_model = load_classifier_free_model(whole_img_model_path, channels=1, num_classes=3)
+        whole_image_model = load_classifier_free_model(WH_PATH, channels=1, num_classes=3)
         img_class = 0
     else:
-        whole_image_model = load_model(whole_img_model_path, channels=1)
+        whole_image_model = load_model(WH_PATH, channels=1)
         img_class = ''
 
     # load model for generating local contexts (mid-resolution images)
-    local_context_model_path = '../models/artifacts/vindr_lcl_ctx_3072:v37/model_56999.pt'
-    local_context_model = load_model(local_context_model_path, channels=3)
+    local_context_model = load_model(LC_PATH, channels=3)
 
     # load model for generating patches in native resolution
-    patch_model_path = '../models/artifacts/vindr_3c_256_v2:v84/model_169999.pt'
-    patch_model = load_model(patch_model_path, channels=3)
+    patch_model = load_model(PH_PATH, channels=3)
 
     # MAMBO pipeline - generate new images in full resolution
     index=0
     num_images = 200
-    base_dir = f'/lustre/seed/vindr_birads_cond/{str(img_class)}'
+    base_dir = f'{SAVE_DIR}{str(img_class)}'
     while index < num_images:
         save_dir = os.path.join(base_dir, str(index))
         lc_dir = os.path.join(save_dir, 'local_contexts')
@@ -65,7 +62,7 @@ if __name__ == "__main__":
         inputs, black_idx = create_inputs(img, img_channels, patch_coords, mask_shape=FINAL_IMAGE_SIZE)
         patches = generate_patches(patch_model, inputs, black_idx, timesteps=timesteps, overlap=OVERLAP, device=device)
         final_img = stitch_patches(patches, overlap=OVERLAP)
-        save_image_to_dir(final_img, str(Path(save_dir) / f'whole_middle.png'))
+        save_image_to_dir(final_img, str(Path(save_dir) / f'whole_large.png'))
         save_patches_to_dir(patches, patches_dir)
 
         print(f'done: {save_dir}')
